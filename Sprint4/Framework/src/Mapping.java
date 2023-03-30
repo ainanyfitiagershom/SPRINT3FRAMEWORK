d9fb6048d87cfde2ce0427219e52e7f2315a6f54
ainanyfitiagershom <118452611+ainanyfitiagershom@users.noreply.github.com>
2023-03-30 22:32:35 +0300

    Sprint4

---
+++ Sprint4/Framework/src/Mapping.java
@@ -0,0 +1,18 @@
+package etu1776.framework;
+public class Mapping{
+    String className;
+    String method;
+
+    public Mapping(){ }
+
+    public Mapping(String className, String method) {
+        this.className = className;
+        this.method = method;
+    }
+    public String getClassName() { return className; }
+    public String getMethod() { return method; }
+
+    public void setClassName(String className) { this.className = className; }
+    public void setMethod(String method) { this.method = method;}
+    
+}
